c1c32fa772518a8119057f3ffdc7304259333f53
Paget Fink <pagetfink@gmail.com>
2015-02-02 14:22:15 -0500

    showing beginning page1

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 $(function () {
   var page1Html = $('#page1').html();
   var page2Html = $('#page2').html();
-})
 
 
 
-$(function () {
   $('.spa-content').html(page1Html);
 
   $('.panel-1').click(function () {
