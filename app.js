35f4546cf7942451761a49b6df2aac6502952310
Paget Fink <pagetfink@gmail.com>
2015-02-02 20:26:21 -0500

    added button toggle between views

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
+// $(function () {
+//   var page1Html = $('#page1').html();
+//   var page2Html = $('#page2').html();
+//
+//
+//
+  // $('.spa-content').html(page1Html);
+
+  // $('.panel-1').click(function () {
+  //   $('.spa-content').html(page2Html);
+  // });
+// });
+
+
 $(function () {
-  var page1Html = $('#page1').html();
-  var page2Html = $('#page2').html();
+  var listPage = $('#list-page').html();
+  var newApptPage = $('#new-appt').html();
 
+  $('.spa-content').html(listPage);
 
+  $('.spa-content').on('click', '.add-appt', function () {
+    $('.spa-content').html(newApptPage);
+    return false;
+  });
 
-  $('.spa-content').html(page1Html);
-
-  $('.panel-1').click(function () {
-    $('.spa-content').html('page2Html');
+  $('.spa-content').on('click', '.show-list-page', function () {
+    $('.spa-content').html(listPage);
+    return false;
   });
 });
